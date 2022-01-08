e0379a7e9d4a8bd0950d3498ac6fe580f0a2fd9d
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-08 14:03:46 -0300

    feat: add global state type

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { IState } from '../store';
+import { ICartItem } from '../store/modules/cart/types';
 
 
 const Cart: React.FC = () => {
-  const state = useSelector(state => state)
-
-  console.log(state);
-  
+  const cart = useSelector<IState, ICartItem[]>(state => state.cart.items)
 
   return (
     <table>
@@ -20,9 +19,16 @@ const Cart: React.FC = () => {
       </thead>
 
       <tbody>
-        <tr>
-          <td></td>
-        </tr>
+        {
+          cart.map((item) => (
+            <tr key={item.product.id}>
+              <td>{item.product.title}</td>
+              <td>{item.product.price}</td>
+              <td>{item.quantity}</td>
+              <td>{(item.quantity * item.product.price).toFixed(2)}</td>
+            </tr>
+          ))
+        }
       </tbody>
     </table>
   );
