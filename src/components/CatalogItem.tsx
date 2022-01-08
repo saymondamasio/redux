af8fc25894e4609bc9790e5361aea3073bc9432e
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-08 20:21:06 -0300

    feat: show out of stock

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { IState } from '../store';
 import { addProductToCartRequest } from '../store/modules/cart/actions';
 import { IProduct } from '../store/modules/cart/types';
 
@@ -11,6 +12,8 @@ interface Props {
 const CatalogItem: React.FC<Props> = ({ product }) => {
   const dispatch = useDispatch()
 
+  const hasFailedStockCheck = useSelector<IState, boolean>(state => state.cart.failedStockCheck.includes(product.id))
+
   const handleAddProductToCart = useCallback(() => {
     dispatch(addProductToCartRequest(product))
   },[dispatch,product])
@@ -21,6 +24,10 @@ const CatalogItem: React.FC<Props> = ({ product }) => {
       <span>{product.price}</span> {' '}
 
       <button type='button' onClick={handleAddProductToCart}>Comprar</button>
+    
+      {
+        hasFailedStockCheck && <span style={{ color: 'red' }}>Produto sem estoque</span>
+      }
     </article>
   );
 }
