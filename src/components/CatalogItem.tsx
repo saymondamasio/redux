3c88748784a6d6d9311d6a41ff127add5b4c6000
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-08 19:59:38 -0300

    feat: split the actions

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import { addProductToCart } from '../store/modules/cart/actions';
+import { addProductToCartRequest } from '../store/modules/cart/actions';
 import { IProduct } from '../store/modules/cart/types';
 
 
@@ -12,7 +12,7 @@ const CatalogItem: React.FC<Props> = ({ product }) => {
   const dispatch = useDispatch()
 
   const handleAddProductToCart = useCallback(() => {
-    dispatch(addProductToCart(product))
+    dispatch(addProductToCartRequest(product))
   },[dispatch,product])
 
   return (
--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,10 +1,17 @@
-import { all, takeLatest } from 'redux-saga/effects';
+import { all, select, takeLatest } from 'redux-saga/effects';
+import { IState } from '../..';
+import { addProductToCartRequest } from './actions';
 
-function checkProductStock() {
-  console.log('Produto adicionado ao carrinho');
-  
+type CheckProductStockRequest = ReturnType<typeof addProductToCartRequest>
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0
+  })
 }
 
 export default all([
-  takeLatest('ADD_PRODUCT_TO_CART', checkProductStock)
+  takeLatest('ADD_PRODUCT_TO_CART_REQUEST', checkProductStock)
 ])
